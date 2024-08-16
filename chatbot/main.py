a2713d7ef0f4e1edf7e49aed2d17764104737fdf
LuizGusta1 <luiz.gustavobr2602@gmail.com>
2024-08-16 10:57:49 -0300

    Criação do modelo

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,21 +1,21 @@
 from chatbot import ChatBot
 myChatBot = ChatBot()
 #apenas carregar um modelo pronto
-#myChatBot.loadModel()
+myChatBot.loadModel()
 
 #criar o modelo
-myChatBot.createModel()
+#myChatBot.createModel()
 
 print("Bem vindo ao Chatbot")
 
 
-pergunta = input("como posso te ajudar?")
+pergunta = input("como posso te ajudar?\n")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("posso lhe ajudar com algo a mais?")
+    pergunta = input("posso lhe ajudar com algo a mais?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
     print(resposta + "   [" + intencao[0]['intent'] + "]")
 
